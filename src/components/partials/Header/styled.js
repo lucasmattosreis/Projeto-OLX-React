81f9b1a7cd7c48a920453cc4dbbd7a6ecb9654fd
Lucas Reis <lucasmattos68@gmail.com>
2021-04-05 15:43:42 -0300

    commit projeto OLX com react

---
+++ src/components/partials/Header/styled.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const HeaderArea = styled.div`
+background-color:#fff;
+height:60px;
+border-bottom:1px solid #CCC;
+
+.container{
+    max-width:1000px;
+    margin:auto;
+    display:flex;
+}
+a {
+    text-decoration:none;
+}
+
+.logo {
+    flex:1;
+    display:flex;
+    align-items:center;
+    height:60px;
+
+    .logo-1,
+    .logo-2,
+    .logo-3,{
+        font-size:27px
+        font-wight:bold;
+    }
+    .logo-1{color:#FF0000; }
+    .logo-2{color:#00FF00; }
+    .logo-3{color:#0000FF; }
+}
+`;
